baadee3c7e53f9e7b5d18eda089b34d8ce600392
Vortex-Overdrive <kkurapati10@gmail.com>
2023-07-07 00:24:49 -0500

    Basic Movement of Snail + Font

--- Start.py
+++ Start.py
@@ -3,10 +3,14 @@ from sys import exit
 pygame.init()
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/ysp.ttf',50)
+snailx = 800
 
 screen = pygame.display.set_mode((800,400))
-sky_surface = pygame.image.load("graphics/Sky.png")
-ground_surface = pygame.image.load('graphics/Ground.png')
+sky_surface = pygame.image.load("graphics/Sky.png").convert_alpha()
+ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
+text_surface = test_font.render("My Game",False,'Black')
+snail_surface = pygame.image.load("graphics/snail/snail1.png",).convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -16,8 +20,11 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+    screen.blit(snail_surface,(snailx,270))
 
-
-
+    snailx -= 4
+    if(snailx < -100):
+        snailx = 800
     pygame.display.update()
     clock.tick(60)

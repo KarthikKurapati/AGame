f617632d5926d558d7fd26e6d7909c113dcca896
Vortex-Overdrive <kkurapati10@gmail.com>
2023-07-07 00:45:52 -0500

    Collisions

--- Start.py
+++ Start.py
@@ -4,13 +4,18 @@ pygame.init()
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/ysp.ttf',50)
-snailx = 800
+
 
 screen = pygame.display.set_mode((800,400))
 sky_surface = pygame.image.load("graphics/Sky.png").convert_alpha()
 ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
 text_surface = test_font.render("My Game",False,'Black')
-snail_surface = pygame.image.load("graphics/snail/snail1.png",).convert_alpha()
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rectangle = snail_surface.get_rect(midbottom = (800,300))
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (50,300))
 
 while True:
     for event in pygame.event.get():
@@ -21,10 +26,17 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    screen.blit(snail_surface,(snailx,270))
 
-    snailx -= 4
-    if(snailx < -100):
-        snailx = 800
+    
+    screen.blit(snail_surface,snail_rectangle)
+    snail_rectangle.x -= 5;
+    if(snail_rectangle.right < -100):snail_rectangle.x= 800
+
+
+    screen.blit(player_surface,player_rectangle)
+
+    if player_rectangle.colliderect(snail_rectangle):
+        print("collision")
+    
     pygame.display.update()
     clock.tick(60)

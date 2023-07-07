a7187ad17fc72d16b9612c90894b223781230a4d
Vortex-Overdrive <kkurapati10@gmail.com>
2023-07-07 00:02:32 -0500

    Pygame Fullyset+ Background

--- Start.py
+++ Start.py
@@ -1,11 +1,23 @@
 import pygame
-
+from sys import exit
 pygame.init()
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
 screen = pygame.display.set_mode((800,400))
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load('graphics/Ground.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit();
+            exit()
 
-    pygame.display.update
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+
+
+
+    pygame.display.update()
+    clock.tick(60)
